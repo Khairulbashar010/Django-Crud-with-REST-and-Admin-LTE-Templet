ab1d80aca2e51b0c560ec237b4c8c5282a907ae1
Khairul Bashar <khairul.bashar.rpsu@gmail.com>
2021-01-12 14:33:48 +0600

    Setup database and migrate

--- django_crud/django_crud/settings.py
+++ django_crud/django_crud/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'django_crud.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_app',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
@@ -123,4 +127,4 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
     '/var/www/static/',
-]
+    ]
