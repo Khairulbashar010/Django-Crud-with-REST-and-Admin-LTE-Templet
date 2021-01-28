4ee5aaaa264424ccb0722f00e3aeee1ef5b26465
Khairul Bashar <khairul.bashar.rpsu@gmail.com>
2021-01-28 13:07:24 +0600

    Update post completed.

--- django_crud/crud/urls.py
+++ django_crud/crud/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.index, name='crud_index'),
     path('create', views.createPage, name='create_page'),
     path('create_post', views.create, name='crud_create'),
+    path('edit/<int:id>', views.editPage, name='edit_page'),
+    path('update/<int:id>', views.update, name='crud_update'),
 ]
--- django_crud/crud/views.py
+++ django_crud/crud/views.py
@@ -25,3 +25,22 @@ def create(request):
     post_info = PostInfo(question=question, answer=answer, isActive=isActive)
     post_info.save()
     return redirect('crud_index')
+
+
+def editPage(request, id):
+    post_by_id = {
+        'post': PostInfo.objects.get(pk=id)
+    }
+    return render(request, 'crud/edit.html', post_by_id)
+
+
+def update(request, id):
+    post_info = PostInfo.objects.get(pk=id)
+    post_info.question = request.POST["question"]
+    post_info.answer = request.POST["answer"]
+    post_info.isActive = request.POST["isActive"]
+    post_info.save()
+
+    return redirect('crud_index')
+
+
